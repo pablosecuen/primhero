a0fbf0fb207294f584e5c6eecca6c641bdfa6d12
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-07-28 17:58:07 -0300

    smooth animation for mobile menu

--- primhero/components/NavbarMenu/NavbarMenu.tsx
+++ primhero/components/NavbarMenu/NavbarMenu.tsx
@@ -1,73 +1,69 @@
+"use client";
 import Image from "next/image";
 import { NavbarMenuProps } from "../../types/NavbarMenuProps";
 import exit from "../../assets/exit/BotonExit.svg";
 import Link from "next/link";
 import handleNavItemClick from "@component/utils/handleNavItemClick";
+import { useEffect } from "react";
 
 const NavbarMenu: React.FC<NavbarMenuProps> = ({ isOpen, toggleMenu }) => {
+  useEffect(() => {
+    const handleEscapeKey = (event: any) => {
+      if (event.key === "Escape") {
+        toggleMenu();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleEscapeKey);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [isOpen, toggleMenu]);
+
   return (
     <>
-      {isOpen && (
-        <>
-          <div
-            className="hover:text-secondary  transition duration-1000 h-screen w-screen fixed top-0 left-0 bg-black/20"
-            onClick={toggleMenu}
-          ></div>
+      <div
+        className={`transition-all duration-500 ${
+          isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+        } fixed h-screen w-screen top-0 left-0 bg-black/20`}
+        onClick={toggleMenu}
+      ></div>
 
-          <div
-            className={`fixed top-12 right-0 w-[50%] h-screen bg-primary shadow-2xl  p-4 transition-transform duration-1000 mt-4 ${
-              isOpen ? "translate-x-0" : "translate-x-full"
-            }`}
-          >
-            {/* <div className="w-full border flex flex-col justify-around items-center align-middle">
-              <Image
-                src={exit}
-                alt="foto"
-                className="rounded-full border"
-                width={50}
-                height={50}
-                layout="fixed" 
-              />
-              <p className="hover:text-secondary transition duration-500">usuario</p>
-              <Image
-                src={exit}
-                alt="exit"
-                className=""
-                width={100}
-                height={100}
-                layout="fixed" 
-              />
-            </div> */}
-
-            <ul className="flex flex-col">
-              <li className="my-2 hover:text-secondary transition duration-700">
-                <Link href="http://localhost:3000/">Inicio</Link>
-              </li>
-              <li className="my-2 hover:text-secondary transition duration-700">
-                <Link href="http://localhost:3000/tracking">Tracking</Link>
-              </li>
-              <li className="my-2 hover:text-secondary transition duration-700">
-                <Link href="#servicios" onClick={(e) => handleNavItemClick(e, "servicios")}>
-                  Servicios
-                </Link>
-              </li>
-              <li className="my-2 hover:text-secondary transition duration-700">
-                <Link href="#nosotros" onClick={(e) => handleNavItemClick(e, "nosotros")}>
-                  Nosotros
-                </Link>
-              </li>
-              <li className="my-2 hover:text-secondary transition duration-700">
-                <Link href="#tutoriales" onClick={(e) => handleNavItemClick(e, "tutoriales")}>
-                  Tutoriales
-                </Link>
-              </li>
-              <Link href="#contacto" onClick={(e) => handleNavItemClick(e, "contacto")}>
-                Contacto
-              </Link>
-            </ul>
-          </div>
-        </>
-      )}
+      <div
+        className={`transition-all duration-500 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } fixed top-12 right-0 w-[50%] h-screen bg-primary shadow-2xl p-4 mt-4`}
+      >
+        <ul className="flex flex-col">
+          <li className="my-2 hover:text-secondary transition duration-500">
+            <a href="http://localhost:3000/">Inicio</a>
+          </li>
+          <li className="my-2 hover:text-secondary transition duration-500">
+            <a href="http://localhost:3000/tracking">Tracking</a>
+          </li>
+          <li className="my-2 hover:text-secondary transition duration-500">
+            <a href="#servicios" onClick={(e) => handleNavItemClick(e, "servicios")}>
+              Servicios
+            </a>
+          </li>
+          <li className="my-2 hover:text-secondary transition duration-500">
+            <a href="#nosotros" onClick={(e) => handleNavItemClick(e, "nosotros")}>
+              Nosotros
+            </a>
+          </li>
+          <li className="my-2 hover:text-secondary transition duration-500">
+            <a href="#tutoriales" onClick={(e) => handleNavItemClick(e, "tutoriales")}>
+              Tutoriales
+            </a>
+          </li>
+          <a href="#contacto" onClick={(e) => handleNavItemClick(e, "contacto")}>
+            Contacto
+          </a>
+        </ul>
+      </div>
     </>
   );
 };
